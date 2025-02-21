9062f84625805141f0eda2f4b7601b7c7a96f07c
Phat Nguyen <phatnguyentanit@gmail.com>
2025-02-20 22:05:06 -0500

    Added comment

--- mygateway/src/main/java/com/mycompany/mygateway/GrantedAuthoritiesExtractor.java
+++ mygateway/src/main/java/com/mycompany/mygateway/GrantedAuthoritiesExtractor.java
@@ -8,6 +8,10 @@ import org.springframework.security.oauth2.jwt.Jwt;
 import java.util.*;
 import java.util.stream.Collectors;
 
+/**
+ * This class is to extract roles from jwt token, the roles are required for authorization
+ * In this demo, I don't implement authorization function
+ */
 public final class GrantedAuthoritiesExtractor implements Converter<Jwt, Collection<GrantedAuthority>> {
 
     public Collection<GrantedAuthority> convert(Jwt jwt) {
